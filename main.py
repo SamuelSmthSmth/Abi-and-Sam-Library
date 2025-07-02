f732c755e7894557f543094886cd217f6d0af07a
Samuel Okwuonu <skelechi07@gmail.com>
2025-07-02 13:21:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
 def menu():
-    print("[INSERT NAME HERE]")
-    print("\nPlease Select an option:" \
-    "\n1.   ")
-
-    
+    print("[INSERT NAME HERE]\n")
+    print("" \
+    "\nPlease Select an option:" \
+    "\n1.   Show Books" \
+    "\n2.   Add Book" \
+    "\n3.   Remove Book" \
+    "\n4.   Exit")
+    choice = int(input())
+    return choice
