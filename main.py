bfdc269ffe86d622a67c1182f52b7602464db9db
abiheda <abisolahd@gmail.com>
2025-07-02 15:55:27 +0100

    lklk

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
 import json
+
 def readbooks():
     with open('Books/list.json', 'r') as f:
         books = json.load(f)
-    with open('Books/list.json', 'a') as f:
-        json.dump(books[0], f)
+    return books
+
+def writebooks(booktags): #booktags is a dict type
+    with open('Books/list.json', 'r+') as f:
+        books = json.load(f) #dict
+        books.append(booktags)
+        print(books)
+        f.seek(0) #moves cursor to the start
+        json.dump(books, f, indent = 2)
+
+
 
 def menu():
-    print("[INSERT NAME HERE]\n")
+    print("Library\n")
     print("" \
     "\nPlease Select an option:" \
     "\n1.   Show Books" \
@@ -18,7 +28,21 @@ def menu():
     return choice
 
 def menu_ft(option):
+    books = readbooks()
     if option == 1:
+        for i in range(len(books)):
+            print(books[i]['title'])
+    elif option == 2:
         pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        pass
+    else:
+        os.system('cls')
+        print("That is not an option")
+    
 
-readbooks()
+while True:
+    menu_ft(menu())
+
