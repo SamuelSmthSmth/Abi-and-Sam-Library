0e608765095e9627c8d694a81fb72201ba06370f
Samuel Okwuonu <skelechi07@gmail.com>
2025-07-04 00:00:59 +0100

    Update practice.ts

--- practice.ts
+++ practice.ts
@@ -1,6 +1,8 @@
 //This is where I'm practicing my typescript
 
-var message = 12345;
-const message = "Hello World";
+const const_1 = "This is a constant."
+let const_2 = "\nThis is a variable."
+//const_1 = "This is the second iteration of a constant"; This will definitely throw an error lol
+const_2 = "\nThis is the second iteration of a variable."
 
-console.log(message)
+console.log(const_1 + const_2)
