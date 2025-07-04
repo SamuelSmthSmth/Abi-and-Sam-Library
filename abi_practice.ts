831a2fc2fc0ae0a2688798def44ca5f99b1940ef
Samuel Okwuonu <skelechi07@gmail.com>
2025-07-04 13:23:55 +0100

    Added Deno

--- abi_practice.ts
+++ abi_practice.ts
@@ -3,3 +3,7 @@ const multiplicator = (a, b, printText) => {
 }
 
 multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+
+//When you get the chance, install deno to run your TypeScript files
+//Use the command 'irm https://deno.land/install.ps1 | iex' in the terminal and restart vscode
+//Then, to run a file, simply type deno with your file name (e.g. deno abi_practice.ts)
