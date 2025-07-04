15b1fed8e28fa2be609288530d507f274aab153d
Samuel Okwuonu <skelechi07@gmail.com>
2025-07-04 11:54:30 +0100

    Just practicing

--- practice.ts
+++
@@ -1,8 +0,0 @@
-//This is where I'm practicing my typescript
-
-const const_1 = "This is a constant."
-let const_2 = "\nThis is a variable."
-//const_1 = "This is the second iteration of a constant"; This will definitely throw an error lol
-const_2 = "\nThis is the second iteration of a variable."
-
-console.log(const_1 + const_2)
