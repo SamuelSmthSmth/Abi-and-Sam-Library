77e745d3887b5fd5f39a9fd4aa0863de1be36890
Samuel Okwuonu <skelechi07@gmail.com>
2025-07-10 20:01:34 +0100

    Moved to Webstorm

--- abi_practice.ts
+++
@@ -1,9 +0,0 @@
-const multiplicator = (a, b, printText) => {
-  console.log(printText,  a * b);
-}
-
-multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
-
-//When you get the chance, install deno to run your TypeScript files
-//Use the command 'irm https://deno.land/install.ps1 | iex' in the terminal and restart vscode
-//Then, to run a file, simply type deno with your file name (e.g. deno abi_practice.ts)
